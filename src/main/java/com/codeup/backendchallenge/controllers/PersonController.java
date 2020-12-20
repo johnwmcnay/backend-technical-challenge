216d4d38bdee6182b125ef460a1778c88867f28a
John McNay <john.w.mcnay@gmail.com>
2020-12-19 23:28:18 -0600

    read one, read all, and add work; still need to do update and delete

--- src/main/java/com/codeup/backendchallenge/controllers/PersonController.java
+++ src/main/java/com/codeup/backendchallenge/controllers/PersonController.java
@@ -2,16 +2,17 @@ package com.codeup.backendchallenge.controllers;
 
 import com.codeup.backendchallenge.models.Person;
 import com.codeup.backendchallenge.repos.PersonRepository;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.assertj.core.util.Preconditions;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/person")
 public class PersonController {
-
 
     private final PersonRepository personDao;
 
@@ -21,5 +22,20 @@ public class PersonController {
         return personDao.getOne(id);
     }
 
+    @GetMapping("/all")
+    public List<Person> getAll() {
+        return personDao.findAll();
+    }
+
+    @PostMapping(value = "/add", consumes = "application/json", produces = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Person addPerson(@RequestBody Person person) {
+        Preconditions.checkNotNull(person);
+        return personDao.save(person);
+    }
+
+    //update
+
+    //delete
 
 }
