c5b31b7e761e994fd10ffda73d77176869ac2d20
John McNay <john.w.mcnay@gmail.com>
2020-12-19 20:06:15 -0600

    Person model with MySQL table created by hibernate

--- src/main/java/com/codeup/backendchallenge/models/Person.java
+++ src/main/java/com/codeup/backendchallenge/models/Person.java
@@ -1,11 +1,35 @@
 package com.codeup.backendchallenge.models;
 
-//Person should have fields
-//        ○ id (auto-increment)
-//        ○ name
-//        ○ age
-//        ○ dateJoined
-//        ○ dateUpdated
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.*;
+import java.sql.Date;
+
+
+@Entity
+@Table(name="persons")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private short age;
+
+    @Column(name = "date_joined")
+    private Date dateJoined;
+
+    @Column(name = "date_updated")
+    private Date dateUpdated;
 }
