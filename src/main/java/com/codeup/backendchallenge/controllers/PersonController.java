cc5f1a4b97301f13dd9e9210628b948959e645f9
johnwmcnay <john.w.mcnay@gmail.com>
2020-12-28 17:21:55 -0600

    feat: added delete and update person

--- src/main/java/com/codeup/backendchallenge/controllers/PersonController.java
+++ src/main/java/com/codeup/backendchallenge/controllers/PersonController.java
@@ -7,6 +7,7 @@ import org.assertj.core.util.Preconditions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 @RestController
@@ -34,9 +35,25 @@ public class PersonController {
         return personDao.save(person);
     }
 
-    //test push
-    //update
+    @PostMapping(value = "{id}/delete")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void deletePerson (@PathVariable(name = "id") long id) {
+        personDao.deleteById(id);
+    }
 
-    //delete
 
+    //TODO: date conversion can result in being off by one day
+    @PostMapping(value = "/{id}/update", consumes = "application/json", produces = "application/json")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public Person updatePerson(@PathVariable(name = "id") long id, @RequestBody Person person) {
+        Preconditions.checkNotNull(person);
+        Person personToUpdate = personDao.getOne(id);
+
+        personToUpdate.setAge(person.getAge() == 0 ? personToUpdate.getAge() : person.getAge());
+        personToUpdate.setName(person.getName() == null ? personToUpdate.getName() : person.getName());
+        personToUpdate.setDateJoined(person.getDateJoined() == null ? personToUpdate.getDateJoined() : person.getDateJoined());
+        personToUpdate.setDateUpdated(person.getDateUpdated() == null ? personToUpdate.getDateUpdated() : person.getDateUpdated());
+
+        return personDao.save(personToUpdate);
+    }
 }
