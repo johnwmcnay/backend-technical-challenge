1c5c59225eb882b4271cf088bd40d3dc507dd0f2
johnwmcnay <john.w.mcnay@gmail.com>
2020-12-30 09:30:06 -0600

    added CRUD to job controller

--- src/main/java/com/codeup/backendchallenge/models/Job.java
+++ src/main/java/com/codeup/backendchallenge/models/Job.java
@@ -28,13 +28,5 @@ public class Job {
     @Column
     private int salary;
 
-//    Add another object, Job and give it the appropriate db relationship to Person, set
-//    up automated tests for this object and it’s endpoints too
-//○ id
-//○ jobTitle
-//○ Salary
-//● Should be able to add a job, get a job, update a job, delete a job, get all jobs
-//            (CRUD)
-//● Should be able to update an existing Persons job
-//● Write a small SQL seeder file and add some Persons and Jobs to the database
+
 }
---
+++ src/main/java/com/codeup/backendchallenge/repos/JobRepository.java
@@ -0,0 +1,10 @@
+package com.codeup.backendchallenge.repos;
+
+import com.codeup.backendchallenge.models.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job, Long> {
+
+}
